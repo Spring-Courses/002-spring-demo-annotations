a30027291c3844624ca5939be19158aecfd35259
hocinebouarara <hocine@univ-tiaret.dz>
2023-02-18 20:06:26 +0100

    @Autowired with custom method (not setter method)

--- src/com/hocinebouarara/springdemo/TennisCoach.java
+++ src/com/hocinebouarara/springdemo/TennisCoach.java
@@ -14,9 +14,15 @@ public class TennisCoach implements Coach {
 		
 	}
 	//define a setter method 
-	@Autowired
 	public void setFortuneService(FortuneService theFortuneService) {
 		System.out.println(">> TennisCoach: inside setFortuneService() method");
+		fortuneService = theFortuneService;
+	}
+	
+	//define a setter method 
+	@Autowired
+	public void doSomeCrazyStuff(FortuneService theFortuneService) {
+		System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
 		fortuneService = theFortuneService;
 	}
 	/*
